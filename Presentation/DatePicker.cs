4b4b7571dc265543bea089b40eaad983820c7115
AngelovdBroek <1055539@hr.nl>
2023-03-22 14:14:52 +0100

    Comments + ask for other date if no movies.
    Added comments to make the code more clear.
    Also added that if for a given date theres no movies it will tell the user there is no movies at that date and to give another date.

--- Presentation/DatePicker.cs
+++ Presentation/DatePicker.cs
@@ -1,37 +1,38 @@
 static class DatePicker
 {
-
-    //This shows the menu. You can call back to this method to show the menu again
-    //after another presentation method is completed.
-    //You could edit this to show different menus depending on the user's role
     static public void Start()
     {
         Console.WriteLine("Type a date you want to see the shows from like: year-month-day");
         string date = Console.ReadLine();
         List<ShowModel> shows = ShowsAccess.LoadAll();
-        //put this in the logic layer
-        // get the films
-        // add hours
+
+        //Display every show thats from the given date.
+        bool emptyOrNot = false;
         foreach (ShowModel show in shows)
         {
+
             if (show.Date == date)
             {
+                emptyOrNot = true;
                 FilmsLogic filmsLogic = new FilmsLogic();
                 FilmModel film = filmsLogic.GetById(show.FilmId);
                 Console.WriteLine($"Room: {show.RoomId}, Date: {show.Date}, Time: {show.Time}, Movie name: {film.Name}.");
             }
         }
-        showChoose(shows, date);
 
-
-
-        // else
-        // {
-        //     Console.WriteLine("Invalid input");
-        //     Start();
-        // }
-
+        //Check to see if theres any shows at that date or not.
+        //if so to be able to choose a show otherwise ask for another date.
+        if (emptyOrNot == true)
+        {
+            showChoose(shows, date);
+        }
+        else
+        {
+            Console.WriteLine("No movies for this date, please choose another date \n");
+            Start();
+        }
     }
+
     public static void showChoose(List<ShowModel> shows, string date)
     {
         Console.WriteLine("To select the specific show you want to see the details of, or reserve seats for. Type (Room number + Time):");
@@ -39,7 +40,11 @@ static class DatePicker
         string[] input = movie.Split(' ');
         Console.WriteLine(Convert.ToInt32(input[0]));
         Console.WriteLine(input[1]);
+
+        //Put the chosen movie into a variable called show.
         ShowModel show = shows.Find(i => i.RoomId == Convert.ToInt32(input[0]) && i.Time == input[1]);
+
+        //Checks if show exists or not, if not ask again, if yes give specific data to the display page of that show/movie..
         if (show == null)
         {
             Console.WriteLine("At this room / time combination is no movie, try again.");
