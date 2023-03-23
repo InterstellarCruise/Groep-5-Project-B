33cc32fb40bfd8ee34254a2036572601d7f695b7
InterstellarCruise <1063224@hr.nl>
2023-03-23 15:50:43 +0100

    oops
    aaaa

--- Presentation/ChangeShows.cs
+++ Presentation/ChangeShows.cs
@@ -3,6 +3,6 @@ public class ChangeShows
     public static void Start()
     {
         Console.WriteLine("These are all the shows in the system right now");
-        Console.WriteLine(AccountsLogic.CurrentAccount);
+        Console.WriteLine(ShowsAccess.LoadAll());
     }
 }
