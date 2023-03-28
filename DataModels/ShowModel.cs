074432310bc9cc3105c6358e647663693b146d5c
Undertaker <124693454+Undertaker2104@users.noreply.github.com>
2023-03-28 13:28:10 +0200

    a
    a

--- DataModels/ShowModel.cs
+++ DataModels/ShowModel.cs
@@ -9,23 +9,18 @@ class ShowModel
     [JsonPropertyName("filmId")]
     public int FilmId { get; set; }
 
-    [JsonPropertyName("roomId")]
-    public int RoomId { get; set; }
+    [JsonPropertyName("dateTime")]
+    public string DateTime { get; set; }
 
-    [JsonPropertyName("date")]
-    public string Date { get; set; }
-
-    [JsonPropertyName("time")]
-    public string Time { get; set; }
-
-
-    public ShowModel(int id, int filmId, int roomId, string date, string time)
+    public ShowModel(int id, int filmId, string dateTime)
     {
         Id = id;
         FilmId = filmId;
-        RoomId = roomId;
-        Date = date;
-        Time = time;
+        DateTime = dateTime;
     }
 
 }
+
+
+
+
