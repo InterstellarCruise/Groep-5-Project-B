a70c87705149debb35b595189ea53e6255c6e713
AngelovdBroek <1055539@hr.nl>
2023-03-28 14:01:28 +0200

    Merged

--- Presentation/Menu.cs
+++ Presentation/Menu.cs
@@ -27,44 +27,14 @@ static class Menu
         List<MenuItem> items = new List<MenuItem>();
         if (!LoggedIn)
         {
-<<<<<<< HEAD
-            if (input == "1")
-            {
-                Console.Clear();
-                UserLogin.Start();
-            }
-            else if (input == "2")
-            {
-                Console.Clear();
-                DatePicker.Start();
-            }
-            else if (input == "3")
-            {
-                Console.Clear();
-                CinemaInfo.start();
-            }
-            else if (input == "Q" || input == "q")
-            {
-                Environment.Exit(0);
-            }
-            else
-            {
-                Console.WriteLine("Invalid input");
-                int milliseconds = 3000;
-                Thread.Sleep(milliseconds);
-                Console.Clear();
-                Start();
-            }
-=======
             items.Add(new MenuItem("Login/register", UserLogin.Start));
->>>>>>> main
         }
         else if (LoggedIn)
         {
             items.Add(new MenuItem("Account", NotImplemented));
         }
-        items.Add(new MenuItem("Shows", NotImplemented));
-        items.Add(new MenuItem("Cinema Info", NotImplemented));
+        items.Add(new MenuItem("Shows", DatePicker.Start));
+        items.Add(new MenuItem("Cinema Info", CinemaInfo.start));
         if (AdminLogged)
         {
             items.Add(new MenuItem("Admin Features", AdminFeatures.Start));
