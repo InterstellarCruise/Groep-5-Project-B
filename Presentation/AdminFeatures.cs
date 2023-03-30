b8feb31bba09d4fc99a1bda61c3ffec04b0c410e
InterstellarCruise <1063224@hr.nl>
2023-03-30 15:27:46 +0200

    Update admin feat

--- Presentation/AdminFeatures.cs
+++ Presentation/AdminFeatures.cs
@@ -9,7 +9,7 @@ public static class AdminFeatures
     public static void Choices()
     {
         List<MenuItem> items = new List<MenuItem>();
-        items.Add(new MenuItem("Change show details", ChangeShows.Start));
+        items.Add(new MenuItem("Change show or film details", ChangeShows.Start));
         items.Add(new MenuItem("Remove shows or films", RemoveShows.Start));
         items.Add(new MenuItem("Back", Menu.Start));
         items.Add(new MenuItem("Quit", Menu.Quit));
