d422fecd76dfb456b97d4520aeb5333376283900
Undertaker <124693454+Undertaker2104@users.noreply.github.com>
2023-04-04 15:45:38 +0200

    datamodels chair en room

---
+++ DataModels/ChairModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json.Serialization;
+
+
+public class ChairModel
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("roomid")]
+    public int Room { get; set; }
+
+    [JsonPropertyName("rank")]
+    public int Rank { get; set; }
+
+    [JsonPropertyName("row")]
+    public string Row { get; set; }
+
+    [JsonPropertyName("column")]
+    public int Column { get; set; }
+
+
+    public ChairModel(int id, int room, int rank, int column, string row)
+    {
+        Id = id;
+        Room = room;
+        Rank = rank;
+        Column = column;
+        Row = row;
+    }
+
+}
