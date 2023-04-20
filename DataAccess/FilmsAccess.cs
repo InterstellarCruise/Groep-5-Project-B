932d659c1cea5941c2f3149b69062e310c3613d8
InterstellarCruise <1063224@hr.nl>
2023-04-20 11:21:15 +0200

    Update FilmsAccess.cs

--- DataAccess/FilmsAccess.cs
+++ DataAccess/FilmsAccess.cs
@@ -19,6 +19,12 @@ public static class FilmsAccess
         File.WriteAllText(path, json);
     }
 
+    public static void Add(FilmModel film)
+    {
+        var films = LoadAll();
+        films.Add(film);
+        WriteAll(films);
+    }
 
 
 }
