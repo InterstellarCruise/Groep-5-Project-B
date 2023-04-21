c4887a6091ce831699c98c1c7955a591867b4359
AngelovdBroek <1055539@hr.nl>
2023-04-21 10:10:27 +0200

    Added so you can see the show and film info of a reservation

--- Presentation/ReservationList.cs
+++ Presentation/ReservationList.cs
@@ -1,35 +1,34 @@
 public class ReservationList
 {
-    private static ShowModel _show = new ShowModel(0, 0, 0, null, null);
-    private static FilmModel _film = new FilmModel(0, null, null, 0, 0, null);
-    static ShowsLogic showLogic = new ShowsLogic();
-    static FilmsLogic filmLogic = new FilmsLogic();
-    public static ShowModel Show
-    {
-        get { return _show; }
-        set { _show = value; }
-    }
-    public static FilmModel film
-    {
-        get { return _film; }
-        set { _film = value; }
-    }
+    // private static ShowModel _show = new ShowModel(0, 0, 0, null, null);
+    // private static FilmModel _film = new FilmModel(0, null, null, 0, 0, null);
+    // static ShowsLogic showLogic = new ShowsLogic();
+    // static FilmsLogic filmLogic = new FilmsLogic();
+    // public static ShowModel Show
+    // {
+    //     get { return _show; }
+    //     set { _show = value; }
+    // }
+    // public static FilmModel film
+    // {
+    //     get { return _film; }
+    //     set { _film = value; }
+    // }
     static ReservationsLogic resLogic = new ReservationsLogic();
     private static TheReservationModel? _res { get; set; }
     public static void listReservations()
     {
-        Console.Clear();
-        // Console.CursorVisible = false;
-        Console.WriteLine($"Reservations\n");
         int id = AccountsLogic.CurrentAccount.Id;
-        Console.WriteLine(id);
-        ReservationsLogic.AllCurrentRes(id);
-        _res = resLogic.GetById(id);
-        Show = showLogic.GetById(_res.Showid);
-        film = filmLogic.GetById(Show.FilmId);
- 
-        
-
+        List<TheReservationModel> reservations = ReservationsLogic.ReservationsByAccount(id);
+        ShowsLogic showsLogic = new ShowsLogic();
+        FilmsLogic filmsLogic = new FilmsLogic();
+        Console.WriteLine($"Reservations\n");
+        foreach (TheReservationModel reservation in reservations)
+        {
+            ShowModel Show = showsLogic.GetById(reservation.Showid);
+            FilmModel Film = filmsLogic.GetById(Show.FilmId);
+            Console.WriteLine($"Reservation for {Film.Name} at {Show.Date}");
+        }
     }
 }
 
