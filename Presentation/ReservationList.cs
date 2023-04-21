ce909908d9a6931f8522cf8694c6799eec7323ab
AngelovdBroek <1055539@hr.nl>
2023-04-21 10:16:41 +0200

    Update ReservationList.cs

--- Presentation/ReservationList.cs
+++ Presentation/ReservationList.cs
@@ -1,28 +1,12 @@
 public class ReservationList
 {
-    // private static ShowModel _show = new ShowModel(0, 0, 0, null, null);
-    // private static FilmModel _film = new FilmModel(0, null, null, 0, 0, null);
-    // static ShowsLogic showLogic = new ShowsLogic();
-    // static FilmsLogic filmLogic = new FilmsLogic();
-    // public static ShowModel Show
-    // {
-    //     get { return _show; }
-    //     set { _show = value; }
-    // }
-    // public static FilmModel film
-    // {
-    //     get { return _film; }
-    //     set { _film = value; }
-    // }
-    static ReservationsLogic resLogic = new ReservationsLogic();
-    private static TheReservationModel? _res { get; set; }
     public static void listReservations()
     {
+        Console.Clear();
         int id = AccountsLogic.CurrentAccount.Id;
         List<TheReservationModel> reservations = ReservationsLogic.ReservationsByAccount(id);
         ShowsLogic showsLogic = new ShowsLogic();
         FilmsLogic filmsLogic = new FilmsLogic();
-        Console.Clear();
         Console.WriteLine($"Reservations\n");
         Console.WriteLine("=============================================");
         foreach (TheReservationModel reservation in reservations)
